a7e256d02bc50f2f771bcc9af33fa76e4fcaa365
Alexandru Negoita <134933269+Xenoname19@users.noreply.github.com>
2023-06-23 08:55:30 +0300

    Create update_website.sh

---
+++ .github/workflows/update_website.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Define the local and remote branch names
+##local_branch="main"
+##remote_branch="origin/main"
+
+# Pull the latest changes from the remote repository
+##git pull
+
+# Get the latest commit hash
+##latest_commit=$(git rev-parse HEAD)
+
+# Verify if the local branch is up to date with the remote branch
+##if [ "$(git rev-parse $local_branch)" != "$(git rev-parse $remote_branch)" ]; then
+    ##echo "Local branch is not up to date. Pushing changes..."
+
+    # Push the local changes to the remote branch
+    ##git push origin $local_branch
+
+    ##echo "Changes pushed successfully!"
+##else
+    ##echo "Local branch is up to date."
+##fi
+
+# Deploy the updates to your website (replace with your deployment command)
+##echo "Deploying updates to the website..."
+# Your deployment command goes here
+##echo "Updates deployed successfully!"
+
