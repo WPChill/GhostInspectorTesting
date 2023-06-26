e0c729b7e8c5c2e8ba5b7d72bfec0703aca3b013
Alexandru Negoita <134933269+Xenoname19@users.noreply.github.com>
2023-06-26 09:33:39 +0300

    update_website.sh
    Removed commented lines

--- .github/workflows/update_website.sh
+++ .github/workflows/update_website.sh
@@ -1,31 +1,31 @@
-##!/bin/bash
+!/bin/bash
 
 # Define the local and remote branch names
-##local_branch="main"
-##remote_branch="origin/main"
+local_branch="main"
+remote_branch="origin/main"
 
 # Pull the latest changes from the remote repository
-##git pull
+git pull
 
 # Get the latest commit hash
-##latest_commit=$(git rev-parse HEAD)
+latest_commit=$(git rev-parse HEAD)
 
 # Verify if the local branch is up to date with the remote branch
-##if [ "$(git rev-parse $local_branch)" != "$(git rev-parse $remote_branch)" ]; then
-    ##echo "Local branch is not up to date. Pushing changes..."
+if [ "$(git rev-parse $local_branch)" != "$(git rev-parse $remote_branch)" ]; then
+    echo "Local branch is not up to date. Pushing changes..."
 
     # Push the local changes to the remote branch
-    ##git push origin $local_branch
+    git push origin $local_branch
 
-    ##echo "Changes pushed successfully!"
-##else
-    ##echo "Local branch is up to date."
-##fi
+    echo "Changes pushed successfully!"
+else
+    echo "Local branch is up to date."
+fi
 
 # Deploy the updates to your website (replace with your deployment command)
-##echo "Deploying updates to the website..."
+echo "Deploying Modula-lite test updates to the test website..."
 # Your deployment command goes here
-##echo "Updates deployed successfully!"
+echo "Modula-lite test update deployed successfully!"
 
 # This file needs to be made executable with  chmod +x update_website.sh
 # To run the script :  ./update_website.sh
