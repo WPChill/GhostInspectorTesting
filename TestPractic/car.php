fa0686d9d4c58201eb3054f0743da01467aec383
Alexandru Negoita <134933269+Xenoname19@users.noreply.github.com>
2023-07-25 10:05:17 +0300

    Update car.php
    Adding prefixes and escapes for best practice

--- TestPractic/car.php
+++ TestPractic/car.php
@@ -4,7 +4,7 @@
  * Description: Test Practice WordPress
  */
 
-function custom_car_post_type()
+function cpt_custom_car_post_type()
 {
     $labels = array(
         'name' => 'Cars',
@@ -37,28 +37,42 @@ function custom_car_post_type()
 
     register_post_type('car', $args);
 }
-add_action('init', 'custom_car_post_type');
+add_action('init', 'cpt_custom_car_post_type');
 
-function add_custom_fields_to_car_posts()
+function cpt_add_custom_fields_to_car_posts()
 {
     $car_posts = get_post(array('post_type'=>'car','posts_per_page'=>-1,'fields'=>'ids',));
     $fuel_options = array('Gasoline', 'Diesel', 'Electric', 'GPL');
     $manufacturer_options = array('VW', 'Renault', 'Mercedes');
     $color_options = array('Red', 'Blue', 'Green','Yellow');
-    function car_custom_fields_meta_box()
+
+    foreach ($car_posts as $post_id) {
+        if (get_post_type($post_id) == 'car') {
+            $selected_fuel = $fuel_options[array_rand($fuel_options)];
+            add_post_meta($post_id, 'fuel', $selected_fuel, true) or update_post_meta($post_id, 'fuel', $selected_fuel);
+
+            $selected_manufacturer = $manufacturer_options[array_rand($manufacturer_options)];
+            add_post_meta($post_id, 'manufacturer', $selected_manufacturer, true) or update_post_meta($post_id, 'manufacturer', $selected_manufacturer);
+
+            $selected_color = $color_options[array_rand($color_options)];
+            add_post_meta($post_id, 'color', $selected_color, true) or update_post_meta($post_id, 'color', $selected_color);
+        }
+    }
+
+    function cpt_car_custom_fields_meta_box()
     {
         add_meta_box(
-            'car_custom_fields_meta_box',
+            'cpt_car_custom_fields_meta_box',
             'Car Custom Fields',
-            'car_custom_fields_meta_box_callback',
+            'cpt_car_custom_fields_meta_box_callback',
             'home',
             'normal',
-            'high'
+            'low'
         );
     }
-    function car_custom_fields_meta_box_callback($post)
+    add_action('add_meta_boxes', 'cpt_car_custom_fields_meta_box');
+    function cpt_car_custom_fields_meta_box_callback($post)
     {
-
         $fuel_value = get_post_meta($post->ID, 'fuel', true);
         $manufacturer_value = get_post_meta($post->ID, 'manufacturer', true);
         $color_value = get_post_meta($post->ID, 'color', true);
@@ -80,12 +94,11 @@ function add_custom_fields_to_car_posts()
         </table>
         <?php
     }
-    function car_save_custom_fields($post_id)
+    function cpt_car_save_custom_fields($post_id)
     {
         if (wp_is_post_autosave($post_id) || wp_is_post_revision($post_id)) {
             return;
         }
-
 
         if (isset($_POST['fuel_field'])) {
             $fuel_value = sanitize_text_field($_POST['fuel_field']);
@@ -102,38 +115,27 @@ function add_custom_fields_to_car_posts()
             update_post_meta($post_id, 'color', $color_value);
         }
     }
-            foreach ($car_posts as $post_id) {
-                if (get_post_type($post_id) == 'car') {
-                    $selected_fuel = $fuel_options[array_rand($fuel_options)];
-                    add_post_meta($post_id, 'fuel', $selected_fuel, true) or update_post_meta($post_id, 'fuel', $selected_fuel);
-
-                    $selected_manufacturer = $manufacturer_options[array_rand($manufacturer_options)];
-                    add_post_meta($post_id, 'manufacturer', $selected_manufacturer, true) or update_post_meta($post_id, 'manufacturer', $selected_manufacturer);
-
-                    $selected_color = $color_options[array_rand($color_options)];
-                    add_post_meta($post_id, 'color', $selected_color, true) or update_post_meta($post_id, 'color', $selected_color);
-        }
-    }
 }
-function carlist_shortcode($atts)
+
+function cpt_carlist_shortcode($atts)
 {
     $args = shortcode_atts(
-        array('fuel' => '','manufacturer' => '','color' => '','showfilters' => 1,),
+        array('fuel' => '', 'manufacturer' => '', 'color' => '', 'showfilters' => 1),
         $atts
     );
 
     $car_query_args = array(
         'post_type' => 'car',
         'posts_per_page' => -1,
-        'meta_query'=>array(),
+        'meta_query' => array(),
     );
-if(!empty($args['fuel'])){
-    $car_query_args['meta_query']=array(
-     'post_type'=>'car',
-     'posts_per_page'=>-1,
-     "meta_query"=>array(),
-    );
-}
+    if (!empty($args['fuel'])) {
+        $car_query_args['meta_query'][] = array(
+            'key' => 'fuel',
+            'value' => $args['fuel'],
+            'compare' => 'LIKE'
+        );
+    }
 if(!empty($args['manufacturer'])){
     $car_query_args['meta_query'][]=array(
             'key'=>'manufacturer',
@@ -161,12 +163,12 @@ if(!empty($args['manufacturer'])){
             $car_query->the_post();
             $output .= '<article id="post-' . get_the_ID() . '" >
                 <header class="entry-header">
-                    <h2 class="entry-title">' . get_the_title() . '</h2>
+                    <h2 class="entry-title">' . esc_html(get_the_title()) . '</h2>
                 </header>
-                <div class="entry-content">' . get_the_content() . '                    
-                    <p>Fuel: ' . get_post_meta(get_the_ID(), 'fuel', true) . '</p>
-                    <p>Manufacturer: ' . get_post_meta(get_the_ID(), 'manufacturer', true) . '</p>
-                    <p>Color: ' . get_post_meta(get_the_ID(), 'color', true) . '</p>
+                <div class="entry-content">' . wp_kses_post(get_the_content()) . '
+                    <p>Fuel: ' . esc_html(get_post_meta(get_the_ID(), 'fuel', true)) . '</p>
+                    <p>Manufacturer: ' . esc_html(get_post_meta(get_the_ID(), 'manufacturer', true)) . '</p>
+                    <p>Color: ' . esc_html(get_post_meta(get_the_ID(), 'color', true)) . '</p>
                 </div>
             </article>';
         }
@@ -177,13 +179,13 @@ if(!empty($args['manufacturer'])){
     return $output;
 }
 
-
-function enqueue_parent_styles(): void
+function cpt_enqueue_parent_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
-add_action('init', 'add_custom_fields_to_car_posts');
-add_action('add_meta_boxes', 'car_custom_fields_meta_box');
-add_action('save_post', 'car_save_custom_fields');
-add_shortcode('carlist', 'carlist_shortcode');
-add_action('wp_enqueue_scripts', 'enqueue_parent_styles');
+add_action('init', 'cpt_add_custom_fields_to_car_posts');
+add_action('add_meta_boxes', 'cpt_car_custom_fields_meta_box');
+add_action('save_post', 'cpt_car_save_custom_fields');
+add_shortcode('carlist', 'cpt_carlist_shortcode');
+add_action('wp_enqueue_scripts', 'cpt_enqueue_parent_styles');
+
